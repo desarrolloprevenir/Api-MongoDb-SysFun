448d97d8ea0a9962feac7b5c6488b12a1487ae7c
GeovanyOjeda14 <geovanyojeda14@gmail.com>
2020-06-29 20:51:10 -0500

    listo crud convenios, listo para empezar planes

--- routes/convenio.js
+++ routes/convenio.js
@@ -17,6 +17,7 @@ app.post('/', middleware.verificaToken, (req, res) => {
 
     puede.then(() => {
 
+            // console.log(req.body);
             var convenio = new Convenio({
                 nombreEmpresa: req.body.nombreEmpresa,
                 direccionEmpresa: req.body.direccionEmpresa,
@@ -65,7 +66,7 @@ app.post('/', middleware.verificaToken, (req, res) => {
 // Get convenios segun empresa
 // ======================================
 
-app.get('/:idEmpresa', middleware.verificaToken, (req, res) => {
+app.get('/convenios/:idEmpresa', middleware.verificaToken, (req, res) => {
 
     // Verificar permisos
     let puede = permisos.verificarPermisos({ idUsuario: req.usuario.idUsuario, modulo: 1, subMenu: 1, permiso: 'ver' });
@@ -99,6 +100,144 @@ app.get('/:idEmpresa', middleware.verificaToken, (req, res) => {
                     });
 
                 });
+
+        })
+        .catch(mensaje => {
+            res.status(mensaje.status).json(mensaje.json);
+        });
+
+});
+
+// ======================================
+// Get convenios segun empresa
+// ======================================
+
+app.get('/:idConvenio', middleware.verificaToken, (req, res) => {
+    let puede = permisos.verificarPermisos({ idUsuario: req.usuario.idUsuario, modulo: 1, subMenu: 1, permiso: 'ver' });
+    // console.log(req.params.idConvenio);
+    puede.then(() => {
+
+            Convenio.findById(req.params.idConvenio, (err, convenioBD) => {
+
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        mensaje: 'Error al buscar convenio.',
+                        errors: err
+                    });
+                }
+
+                if (!convenioBD) {
+                    return res.status(204).json({
+                        ok: true,
+                        mensaje: 'No hay ningun convenio registrado con este id ' + req.params.idConvenio,
+                    });
+                }
+
+                res.status(200).json({
+                    ok: true,
+                    convenio: convenioBD
+                });
+
+            });
+
+        })
+        .catch(mensaje => {
+            res.status(mensaje.status).json(mensaje.json);
+        });
+});
+
+// ======================================
+// Editar convenio
+// ======================================
+
+app.put('/', middleware.verificaToken, (req, res) => {
+
+    let puede = permisos.verificarPermisos({ idUsuario: req.usuario.idUsuario, modulo: 1, subMenu: 1, permiso: 'editar' });
+    // console.log(req.body);
+    puede.then(() => {
+
+            Convenio.findOneAndUpdate({ _id: req.body._id }, {
+                nombreEmpresa: req.body.nombreEmpresa,
+                direccionEmpresa: req.body.direccionEmpresa,
+                nit: req.body.nit,
+                codigoNit: req.body.codigoNit,
+                telefono: req.body.telefono,
+                correoEmpresa: req.body.correoEmpresa,
+                nombreRepresentanteLegal: req.body.nombreRepresentanteLegal,
+                cedulaRepresentanteLegal: req.body.cedulaRepresentanteLegal,
+                telefonoRepresentanteLegal: req.body.telefonoRepresentanteLegal,
+                fechaNacimientoRepresentanteLegal: req.body.fechaNacimientoRepresentanteLegal,
+                estado: req.body.estado,
+                nombresContacto: req.body.nombresContacto,
+                cargoContacto: req.body.cargoContacto,
+                direccionContacto: req.body.direccionContacto,
+                telefonoContacto: req.body.telefonoContacto,
+                correoContacto: req.body.correoContacto,
+                fechaNacimientoContacto: req.body.fechaNacimientoContacto,
+            }, { new: true }, (err, convenioEdit) => {
+
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        mensaje: 'Error al buscar convenio.',
+                        errors: err
+                    });
+                }
+
+                if (!convenioEdit) {
+                    return res.status(204).json({
+                        ok: true,
+                        mensaje: 'No hay ningun convenio registrado con este id ' + req.params.idConvenio,
+                    });
+                }
+
+                res.status(200).json({
+                    ok: true,
+                    convenio: convenioEdit
+                });
+
+            });
+        })
+        .catch(mensaje => {
+            res.status(mensaje.status).json(mensaje.json);
+        });
+
+});
+
+// ======================================
+// Eliminar convenio
+// ======================================
+
+app.delete('/:idConvenio', middleware.verificaToken, (req, res) => {
+
+    let puede = permisos.verificarPermisos({ idUsuario: req.usuario.idUsuario, modulo: 1, subMenu: 1, permiso: 'eliminar' });
+
+    puede.then(() => {
+
+            Convenio.findByIdAndDelete(req.params.idConvenio, (err, convenioEliminado) => {
+
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        mensaje: 'Error al buscar convenio.',
+                        errors: err
+                    });
+                }
+
+                if (!convenioEliminado) {
+                    return res.status(204).json({
+                        ok: true,
+                        mensaje: 'No hay ningun convenio registrado con este id ' + req.params.idConvenio,
+                    });
+                }
+
+                res.status(200).json({
+                    ok: true,
+                    convenio: convenioEliminado
+                });
+
+            });
 
         })
         .catch(mensaje => {
